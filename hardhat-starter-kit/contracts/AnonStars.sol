eb6890efe3eff427d372b3ab51061da50d14cd5b
Ira <iraf333@gmail.com>
2022-09-03 18:39:08 -0400

    work on main .sol contract

--- hardhat-starter-kit/contracts/AnonStars.sol
+++ hardhat-starter-kit/contracts/AnonStars.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.7;
 
 // 3. Interfaces, Libraries, Contracts, Errors
 //error error_notenougheth();
+error YouAreNotThatGuy_Owner();
 
 /**@title anon stars main contract
  * @author Stackaccount1
@@ -14,14 +15,24 @@ contract AnonStars {
     //Type Declarations
     // *price converter
     //State Variables
+    address private immutable i_owner;
     // *many
     //Events
     //Modifiers
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner);
+        if (msg.sender != i_owner) revert YouAreNotThatGuy_Owner();
+        _;
+    }
+
     // *owner modifier
     //Functions
     // *create profile - maybe superfluid send stake fee back over time, or time lock stake with VRF
     // *endorse profile - maybe superfluid send stake fee back over time, or time lock stake with VRF
     //constructor
+    constructor() {
+        i_owner = msg.sender;
+    }
     //recieve
     //fallback
     //external
