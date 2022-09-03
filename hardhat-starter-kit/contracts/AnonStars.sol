751931a440df9b9f5391d118ef858ebd2a50c2ea
Ira <iraf333@gmail.com>
2022-09-02 20:07:52 -0400

    map out structure of smart contract

---
+++ hardhat-starter-kit/contracts/AnonStars.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// 3. Interfaces, Libraries, Contracts, Errors
+//error error_notenougheth();
+
+/**@title anon stars main contract
+ * @author Stackaccount1
+ * @notice This contract is for creating an profile, and endorsing others, must lock money to endorse to prevent spam, payment is sent back over a long period
+ * @dev This implements price feeds as our library
+ */
+
+contract AnonStars {
+    //Type Declarations
+    // *price converter
+    //State Variables
+    //many
+    //Events
+    //Modifiers
+    //owner modifier
+    //Functions
+    // *create profile - maybe superfluid send stake fee back over time, or time lock stake with VRF
+    // *endorse profile - maybe superfluid send stake fee back over time, or time lock stake with VRF
+    //constructor
+    //recieve
+    //fallback
+    //external
+    //public
+    //internal
+    //private
+    //internal
+    //private
+    //view / pure
+}
